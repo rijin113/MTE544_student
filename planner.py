bc99cf69ebefb800af7aa841eea985b4f9163da4
Rijin Muralidharan <rijin113@gmail.com>
2025-11-26 17:52:13 -0500

    decisions and planner done

--- planner.py
+++ planner.py
@@ -45,14 +45,14 @@ class planner:
         endPoseCart = np.array(endPoseCart)[:2]
 
         # TODO: Convert to pixel coordinates using the m_utilites
-        startPose = self.m_utilites...
-        endPose = self.m_utilites...
+        startPose = self.m_utilites.position_2_cell(startPoseCart)
+        endPose = self.m_utilites.position_2_cell(endPoseCart)
 
         # convert to tuple
         startPose = (startPose[0], startPose[1])
         endPose = (endPose[0], endPose[1])
         # TODO: Call the A* search algorithm
-        path = ...
+        path = search(self.costMap, startPose, endPose)
         if path is None:
             return None
         
